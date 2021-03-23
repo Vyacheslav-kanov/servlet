c7f7c88d50c1e0ff554840bc56e3fab886ce8047
Vyacheslav Maratkanov <slava_maratkanov@mail.ru>
2021-03-23 11:33:09 +0500

    load work

---
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,47 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public final class PostRepository {
+  private final List<Post> list = new ArrayList<>();
+  private int counterByListId = 1;
+  public List<Post> all() {
+    return list;
+  }
+
+  public final Optional<Post> getById(long id) {
+    for(Post e: list){
+      if(e.getId() == id) {
+        return Optional.of(e);
+      }
+    }
+    return null;
+  }
+
+  public final Post save(Post post) {
+    if(post.getId() == 0){
+      post.setId(counterByListId);
+      list.add(post);
+      counterByListId++;
+    }else{
+      for(int i = 0; i < list.size(); i++){
+        if(list.get(i).getId() == post.getId()){
+          list.set(i, post);
+        }else{
+          list.add(post);
+        }
+      }
+    }
+    return post;
+  }
+
+  public final void removeById(long id) {
+    for (int i = 0; i < list.size(); i++) {
+      if(list.get(i).getId() == id) list.remove(i);
+    }
+  }
+}
